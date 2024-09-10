9cd7c0b9b328a5c5727024680f17402920ce26f6
unknown <tharangachaminda@gmail.com>
2024-09-10 17:01:34 +1200

    init setup

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>CI/CD with Github Actions</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <h2>Sample web page to demonstrate CI/CD pipeline with Github Actions</h2>
+        <p>Feel free to go to the <a href="https://github.com/tharangachaminda/aws-cicd" target="_blank">Github repository</a> and make changes to see it in action.</p>
+    </div>    
+</body>
+</html>
